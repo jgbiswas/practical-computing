245e9e58ea3e0efb7581ab5b0abb3c379a04a89b
jgbiswas <jgbiswas@gmail.com>
2024-10-27 21:42:45 +0530

    assignment 3

--- notebooks/exchange_rates.py
+++ notebooks/exchange_rates.py
@@ -3,7 +3,16 @@ import requests
 import json
 url = "https://api.frankfurter.app/2024-01-01..2024-01-10"
 params = {"base":"USD", "symbols":"INR"}
-data = requests.get(url, params=params).json()
-print(data)
-for k in data:
-    print(k)
+data = requests.get(url, params=params)
+
+amount = data.json()['amount']
+base = data.json()['base']
+start_date = data.json()['start_date']
+end_date = data.json()['end_date']
+rates = data.json()['rates']
+
+print(amount)
+print(base)
+print(start_date)
+print(end_date)
+print(rates)
